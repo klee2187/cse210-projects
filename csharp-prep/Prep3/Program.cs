66025ffdb62e78e4ce896294e520455d9bd7276f
klee2187 <kkelly6@byui.edu>
2026-03-05 15:54:27 -0700

    finished stretch prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,32 +5,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
+        bool playAgain = true;
 
-        int guess = -1;
-        int tries = 0;
-
-        while (guess != magicNumber)
+        while (playAgain) 
         {
-            Console.Write("What is your guess?");
-            guess = int.Parse(Console.ReadLine());
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
 
-            tries++;
+            int guess = -1;
+            int tries = 0;
 
-            if (guess < magicNumber)
+            while (guess != magicNumber)
             {
-                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                tries++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You got it!");
+                }
             }
-            else if (guess > magicNumber)
+            Console.WriteLine($"It took you {tries} guesses");
+            Console.Write("Would you like to play again (1=Yes, 0=No)? ");
+            int answer = int.Parse(Console.ReadLine());
+
+            if (answer == 0)
             {
-                Console.WriteLine("Lower");
+                playAgain = false;
             }
-            else
-            {
-                Console.WriteLine("You got it!");
-            }
+
         }
-        Console.WriteLine($"It took you {tries} guesses");
     }
 }
