d66e0496a17edbf4c966c910e5228d96320bcb12
klee2187 <kkelly6@byui.edu>
2026-03-17 15:31:54 -0600

    finished learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment1 = new Assignment("Harry Potter", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Tyrion Lancaster", "Geometry", "3.2", "1-20");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Martha May", "Music History", "The Where, the Why, the How, and The Who");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+
+
     }
 }
