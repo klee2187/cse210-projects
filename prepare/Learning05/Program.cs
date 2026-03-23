7afc1aa1b694fed3767144f88a64b3e8d8c73416
klee2187 <kkelly6@byui.edu>
2026-03-23 13:36:40 -0600

    finished prepare 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square sA1 = new Square("blue", 8);
+        shapes.Add(sA1);
+
+        Rectangle rA1 = new Rectangle("yellow", 11, 5);
+        shapes.Add(rA1);
+
+        Circle cA1 = new Circle("purple", 10);
+        shapes.Add(cA1);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The area of the {color} shape is {area}.");
+        }
     }
 }
