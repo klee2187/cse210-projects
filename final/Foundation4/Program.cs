869875d201b9a36230f51d1c16d2dffdeb78aa76
klee2187 <kkelly6@byui.edu>
2026-04-06 23:17:31 -0600

    forgot to put activities in a list

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,16 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
+
         Console.WriteLine("Hello Foundation4 World!");
 
         Running run = new Running(45, "16 Jun 2023", 6.2m, 8.1m, 7.5m);
-
         IndoorBiking bike = new IndoorBiking(30, "01 Feb 2026", 10.5m, 20.0m, 3.3m);
-
         Swimming swim = new Swimming(30, "12 Mar 1999", 2.4m, 4.4m, 14.8m, 40);
 
-        run.GetSummary();
-        bike.GetSummary();
-        swim.GetSummary();
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(run);
+        activities.Add(bike);
+        activities.Add(swim);
+
+        foreach (Activity activity in activities)
+        {
+            activity.GetSummary();
+        }
     }
 }
